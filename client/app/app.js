4b1da81226fcde3d99679818cf296b00a7e31cfe
Marine Guillanton <marine.guillanton@gmail.com>
2015-09-02 13:54:30 -0700

    add client routing

--- client/app/app.js
+++ client/app/app.js
@@ -1,28 +1,74 @@
-var app = angular.module('news', []);
-app.controller('newsCtrl', ['$scope', function($scope) {
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
-  $scope.addPost = function() {
-    if(!$scope.title || $scope.title === '') {
-      return;
-    }
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0
-    });
-    $scope.title = '';
-    $scope.link = '';
-  };
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
-  };
-}]);
+angular.module('thesis', [
+  'thesis.services',
+  'thesis.articles',
+  'thesis.article',
+  'thesis.user',
+  'thesis.auth',
+  'ngRoute'
+])
 
+// see if ui-router better
+
+.config(function($routeProvider, $httpProvider) { // check $httpProvider
+  $routeProvider
+    .when('/signin', {
+      templateUrl: 'app/views/signin.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/signup', {
+      templateUrl: 'app/views/signup.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/articles', {
+      templateUrl: 'app/views/articles.html',
+      controller: 'ArticlesCtrl',
+    })
+    .when('/article', {
+      templateUrl: 'app/views/article.html',
+      controller: 'ArticleCtrl',
+      authenticate: true
+    })
+    .when('/users/profile', {
+      templateUrl: 'app/views/user.html',
+      controller: 'UserCtrl',
+      authenticate: true
+    })
+    .otherwise({
+      redirectTo: '/articles'
+    });
+})
+
+.factory('AttachTokens', function ($window) {
+  // this is an $httpInterceptor
+  // its job is to stop all out going request
+  // then look in local storage and find the user's token
+  // then add it to the header so the server can validate the request
+  var attach = {
+    request: function (object) {
+      var jwt = $window.localStorage.getItem('com.thesis'); // change name
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+
+.run(function ($rootScope, $location, Auth) {
+  // here inside the run phase of angular, our services and controllers
+  // have just been registered and our app is ready
+  // however, we want to make sure the user is authorized
+  // we listen for when angular is trying to change routes
+  // when it does change routes, we then look for the token in localstorage
+  // and send that token to the server to see if it is a real user or hasn't expired
+  // if it's not valid, we then redirect back to signin/signup
+  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      $location.path('/signin');
+    }
+  });
+});
 
 //python -m SimpleHTTPServer
