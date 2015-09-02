877e4b6218087b279e3a7f743b2cd3baaa615b94
Marine Guillanton <marine.guillanton@gmail.com>
2015-09-02 14:03:00 -0700

    add article model using mongoose

---
+++ server/models/articleModel.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+// TODO add required and default if necessary
+// use base url?
+var ArticleSchema = new mongoose.Schema({
+  url: String,
+  tags: String, // change to array
+  uploader: String,
+  dataloc: String,
+  date: String, // put date
+  comments: Array,
+  commentators: Array,
+  popularityIdx: Number,
+  controversyIdx: Number
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
