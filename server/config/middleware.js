48e87af0e5ed3b29374c16fb1cebbfb38fa3087a
Marine Guillanton <marine.guillanton@gmail.com>
2015-09-03 19:27:16 -0700

    configure document router

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -4,7 +4,8 @@ var bodyParser = require('body-parser');
 module.exports = function (app, express) {
   // routers config
   var userRouter = express.Router();
-  var articleRouter = express.Router();
+  var articlesRouter = express.Router();
+  var documentRouter = express.Router();
 
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true})); // in order to get the data from a POST
@@ -12,9 +13,11 @@ module.exports = function (app, express) {
   app.use(express.static(__dirname + '/../../client'));
 
   app.use('/api/users', userRouter); // use user router for all user request
-  app.use('/api/articles', articleRouter); // user link router for link request
- 
+  app.use('/api/articles', articlesRouter);
+  app.use('/api/document', documentRouter);
+
   // inject routers into their respective route files
   require('../controllers/userRoutes.js')(userRouter);
-  require('../controllers/articleRoutes.js')(articleRouter);
+  require('../controllers/articlesRoutes.js')(articlesRouter);
+  require('../controllers/documentRoutes.js')(documentRouter);
 };
