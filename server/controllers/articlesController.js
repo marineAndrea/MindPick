ef4336147324aba4ed55d37c4a7042f5c2dd30cb
Marine Guillanton <marine.guillanton@gmail.com>
2015-09-07 15:45:48 -0700

    waits for user or article to be saved before sending back response to client

--- server/controllers/articlesController.js
+++ server/controllers/articlesController.js
@@ -1,5 +1,5 @@
 // TODO be consistent with sent err
-// if (err) return console.error(err) ?
+// console.error, throw err, next err, fail catch
 
 var Article = require('../models/articleModel.js');
 var User = require('../models/userModel.js');
@@ -7,6 +7,7 @@ var Q = require('q');
 var utils = require('../config/utils.js');
 
 module.exports = {
+  
   allArticles: function(req, res, next) {
   var findAll = Q.nbind(Article.find, Article);
 
@@ -15,40 +16,49 @@ module.exports = {
       res.json(articles);
     })
     .fail(function (error) {
-      next(error);
+      next(error); ///////////Error
     });
   },
 
   uploadArticle: function(req, res, next) {
-    // unable upload article if no url and no tags provided
 
     // if article does not exist
     //   CREATE NEW ARTICLE
     // in any case
     // UPDATE ARTICLEUPLOADERS
     // UPDATE USERSARTICLES
+
     var url = req.body.url;
     var tags = req.body.tags;
     var username = req.body.username;
+
     // check if article exists already
     var findArticle = Q.nbind(Article.findOne, Article);
     findArticle({url: url})
       .then(function (foundArticle) {
         if (!foundArticle) {
           console.log('article does not already exist in database');
-          return utils.createNewArticle(url, tags); //returns a promise
+          return utils.createNewArticle(url, tags);
         } else {
           console.log('article already exists in database');
-          return foundArticle; // not a promise !!!!!!!!!!!!!
+          return foundArticle;
         }
       })
       .then(function (article) {
-        utils.updateArticlesTable(article._id, 'uploaders', username, true);
-        utils.updateUsersTable(username, 'articles', article._id, true);
-        res.json();
+        return utils.updateArticlesTable(article._id, 'uploaders', username, true, next); ///////////Error
       })
-      .catch(function (error) {
-        console.error(error);
+      .then(function (article) {
+        return utils.updateUsersTable(username, 'articles', article._id, true, next); ///////////Error
+      })
+      .then(function (user) {
+        if (!user) {
+          next(err); //Error
+        } else {
+          res.json();
+        }
+      })
+      .catch(function (error) { //Error
+        next(error);
       });
   }
 };
--- server/controllers/documentController.js
+++ server/controllers/documentController.js
@@ -23,16 +23,58 @@ var createComment = function(name, id, opin, src) {
   return comment;
 };
 
+var uploadArticle = function(url, tags, username, next) {
+
+  // if article does not exist
+  //   CREATE NEW ARTICLE
+  // in any case
+  // UPDATE ARTICLEUPLOADERS
+  // UPDATE USERSARTICLES
+
+  var findArticle = Q.nbind(Article.findOne, Article);
+  return findArticle({url: url})
+    .then(function (foundArticle) {
+      if (!foundArticle) {
+        console.log('article does not already exist in database');
+        return utils.createNewArticle(url, tags);
+      } else {
+        console.log('article already exists in database');
+        return foundArticle;
+      }
+    })
+    .then(function (article) {
+      return utils.updateArticlesTable(article._id, 'uploaders', username, true, next); ///////////Error
+    })
+    .then(function (article) {
+      return utils.updateUsersTable(username, 'articles', article._id, true, next); ///////////Error
+    })
+    .catch(function (error) { //Error
+      next(error);
+    });
+};
+
 module.exports = {
 
   commentDocument: function(req, res, next) {
-    // unable comment with no opinion and no source
+
+
+    // only if opinion provided
+    //   UPDATE USEROPINIONS
+    //   update user by pushing comment: {articleID, opinion, controversyIdx} into opinions [] in user table
+    // only if source provided
+    //   UPLOAD ARTICLE
+    // in any case
+    //   UPDATE ARTICLECOMMENTS (set up default opinion to related)
+    //   update article by pushing comment {articleID, username, opinion, source} into comments [] in article table
+    //   UPDATE ARTICLECOMMENTATORS
+    //   update article by pushing username into commentators [] in article table
+    //   UPDATE USERARTICLES
+
 
     // get username from local storage
     var username = req.body.username;
     var opinion = req.body.opinion || "interesting";
     var source = req.body.source;
-    console.log('++++++++++++++++++++++++source', source);
     if (source) {
       var url = req.body.source.url || undefined;
       var tags = req.body.source.tags || undefined;
@@ -45,58 +87,42 @@ module.exports = {
         articleContrIdx = article.controversyIdx;
         comment = createComment(username, articleId, opinion, source);
         belief = createBelief(articleId, opinion, source, articleContrIdx);
-        console.log('comment', comment, 'belief', belief);
-        
-        // if opinion provided
         if (opinion !== "interesting") {
           console.log('opninon provided', opinion);
-          //   UPDATE USEROPINIONS
-          utils.updateUsersTable(username, 'beliefs', belief, false);
+          return utils.updateUsersTable(username, 'beliefs', belief, false, next); ///////////Error
+          // returns a user
         } else {
           console.log('no opinion provided');
         }
-        // if source provided
+      })
+      .then(function (user) {
         if (source) {
-        console.log('source provided', source);
-          // UPLOAD ARTICLE (SOURCE)
-          // if article does not exist
-          //   CREATE NEW ARTICLE
-          var findArticle = Q.nbind(Article.findOne, Article);
-          findArticle({url: url})
-            .then(function (foundArticle) {
-              console.log('foundArticle', foundArticle);
-              if (!foundArticle) {
-                console.log('article does not already exist in database');
-                return utils.createNewArticle(url, tags);
-              } else {
-                console.log('article already exists in database');
-                return foundArticle; // not a promise !!!!!!!!!!!!!
-              }
-            })
-            // in any case
-            .then(function (article) {
-              // UPDATE ARTICLEUPLOADERS
-              utils.updateArticlesTable(article._id, 'uploaders', username, true);
-              // UPDATE USERSARTICLES
-              utils.updateUsersTable(username, 'articles', article._id, true);
-            })
-            .catch(function (error) {
-              console.error(error);
-            });
+          console.log('source provided', source);
+          return uploadArticle(url, tags, username, next);
+          // returns a user
         } else {
           console.log('no source provided');
+          return user;
         }
-        // in any case
-        //   UPDATE ARTICLECOMMENTS
-        utils.updateArticlesTable(articleId, 'comments', comment, false);
-        //   UPDATE ARTICLECOMMENTATORS
-        utils.updateArticlesTable(articleId, 'commentators', username, true);
-        //   UPDATE USERARTICLES (COMMENTED ARTICLE)
-        utils.updateUsersTable(username, 'articles', articleId, true);
-        res.json();
       })
-      .catch(function (error) {
-        console.error(error);
+      .then(function (user) {
+        return utils.updateArticlesTable(articleId, 'comments', comment, false, next);
+      })
+      .then(function (article) {
+        return utils.updateArticlesTable(articleId, 'commentators', username, true, next);
+      })
+      .then(function (article) {
+        return utils.updateUsersTable(username, 'articles', articleId, true, next);
+      })
+      .then(function (user) {
+        if (!user) {
+          next(err); //Error
+        } else {
+          res.json();
+        }
+      })
+      .catch(function (error) { //Error
+        next(error);
       });
   }
 };
