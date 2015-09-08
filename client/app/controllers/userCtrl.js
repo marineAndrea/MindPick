e94a613d7ae280440e5867a781afc7f06abbd4a0
Marine Guillanton <marine.guillanton@gmail.com>
2015-09-08 00:13:01 -0700

    searches by tag, adds checkbox and sorting filters

--- client/app/controllers/userCtrl.js
+++ client/app/controllers/userCtrl.js
@@ -2,11 +2,13 @@
 
 angular.module('thesis.user', [])
 
-.controller('UserCtrl', function ($scope, User, Articles, $location) {
+.controller('UserCtrl', function ($scope, User, Articles, $location, $filter) {
+
   $scope.getUserInfo = function() {
     User.getInfo()
       .then(function (info) {
         $scope.user = info;
+        $scope.user.picture = $scope.user.picture || "https://upload.wikimedia.org/wikipedia/commons/9/9b/Social_Network_Analysis_Visualization.png";
       })
       .catch(function (error) {
         console.log("oops cannot get user's info");
@@ -15,7 +17,6 @@ angular.module('thesis.user', [])
   $scope.getUserInfo();
 
   $scope.article = {};
-  
   $scope.uploadArticle = function() {
     var username = User.getUsername();
     $scope.article.username = username;
@@ -35,9 +36,82 @@ angular.module('thesis.user', [])
       alert("must provide both url and tags");
     }
   };
+  
+  $scope.articlesInMemory = [];
+  $scope.checkUploaded = false;
+  $scope.checkCommented = false;
+  var orderBy = $filter('orderBy');
+
+  $scope.order = function(predicate, reverse) {
+    $scope.userArticles = orderBy($scope.articlesInMemory, predicate);
+  };
+
+  $scope.includeUploaded = function() {
+    if (!$scope.checkUploaded) {
+      $scope.checkUploaded = true;
+      $scope.userArticles = [];
+      if ($scope.checkCommented) {
+        for (var i = 0; i < $scope.articlesInMemory.length; i++) {
+          if ($scope.articlesInMemory[i].commented && $scope.articlesInMemory[i].uploaded) {
+            $scope.userArticles.push($scope.articlesInMemory[i]);
+          }
+        }
+      } else {
+        for (var j = 0; j < $scope.articlesInMemory.length; j++) {
+          if ($scope.articlesInMemory[j].uploaded) {
+            $scope.userArticles.push($scope.articlesInMemory[j]);
+          }
+        }
+      }
+    } else {
+      $scope.checkUploaded = false;
+      if (!$scope.checkcommented) {
+        $scope.userArticles = $scope.articlesInMemory;
+      } else {
+        $scope.userArticles = [];
+        for (var k = 0; k < $scope.articlesInMemory.length; k++) {
+          if ($scope.articlesInMemory[k].commented) {
+            $scope.userArticles.push($scope.articlesInMemory[k]);
+          }
+        }
+      }
+    }
+  };
+  $scope.includeCommented = function() {
+    if (!$scope.checkCommented) {
+      $scope.checkCommented = true;
+      $scope.userArticles = [];
+      if ($scope.checkUploaded) {
+        for (var i = 0; i < $scope.articlesInMemory.length; i++) {
+          if ($scope.articlesInMemory[i].commented && $scope.articlesInMemory[i].uploaded) {
+            $scope.userArticles.push($scope.articlesInMemory[i]);
+          }
+        }
+      } else {
+        for (var j = 0; j < $scope.articlesInMemory.length; j++) {
+          if ($scope.articlesInMemory[j].commented) {
+            $scope.userArticles.push($scope.articlesInMemory[j]);
+          }
+        }
+      }
+    } else {
+      $scope.checkCommented = false;
+      if (!$scope.checkUploaded) {
+        $scope.userArticles = $scope.articlesInMemory;
+      } else {
+        $scope.userArticles = [];
+        for (var k = 0; k < $scope.articlesInMemory.length; k++) {
+          if ($scope.articlesInMemory[k].uploaded) {
+            $scope.userArticles.push($scope.articlesInMemory[k]);
+          }
+        }
+      }
+    }
+  };
 
   $scope.getUserArticles = function() {
     // get username and user's list of articlesId
+
     $scope.userArticles = [];
     User.getInfo()
       .then(function (userInfo) {
@@ -56,11 +130,15 @@ angular.module('thesis.user', [])
                 // if so
                   // push to userArticles
                   $scope.userArticles.push(articles[i]);
-                  articles[i].uploaded = (articles[i].uploaders.indexOf(username) !== -1) ? 'yes' : 'no';
-                  articles[i].commented = (articles[i].commentators.indexOf(username) !== -1) ? 'yes' : 'no';
+                  articles[i].uploaded = (articles[i].uploaders.indexOf(username) !== -1) ? 1 : 0;
+                  articles[i].commented = (articles[i].commentators.indexOf(username) !== -1) ? 1 : 0;
                 }
               }
             }
+            $scope.articlesInMemory = $scope.userArticles;
+          })
+          .then(function () {
+            $scope.order('-popularityIdx',false); // change to date but change date format first
           })
           .catch(function (error) {
             console.log("oops cannot get user's articles");
@@ -70,5 +148,7 @@ angular.module('thesis.user', [])
         console.log("oops cannot get user's info");
       });
   };
+
   $scope.getUserArticles();
+
 });
