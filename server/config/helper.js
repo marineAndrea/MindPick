816670ecb887586d1d770eb44ef11b0d654c818c
Marine Guillanton <marine.guillanton@gmail.com>
2015-09-10 17:28:34 -0700

    add helper file to update articles' popularity, create a comment, create a belief etc.

---
+++ server/config/helper.js
@@ -0,0 +1,159 @@
+var Article = require('../models/articleModel.js');
+var User = require('../models/userModel.js');
+var Q = require('q');
+var utils = require('./utils.js');
+
+var getAllArticles = function() {
+  var findAll = Q.nbind(Article.find, Article);
+  return findAll({});
+};
+
+saveTable = function(table) {
+  console.log('=============================================about to save table', table);
+  var save = Q.nbind(table.save, table);
+  return save();
+};
+
+var updateAllArticlesPopularity = function(idx, val) {
+  var min, max;
+  getAllArticles()
+    .then(function (articles) {
+      for (var i = 0; i < articles.length; i++) {
+        if (idx === 0) {
+          min = val;
+          max = articles[i].minmaxComments[1];
+        } else if (idx === 1) {
+          max = val;
+          min = articles[i].minmaxComments[0];
+        }
+        articles[i].minmaxComments = [min, max];
+        articles[i].popularityIdx = getPopularity(articles[i], 10, 'wat');
+        saveTable(articles[i]); // do not need to wait for this
+      }
+    });
+  return;
+};
+
+var getPopularity = function(article, scale) {
+  var nbComments = article.comments.length;
+  var min = article.minmaxComments[0];
+  var max = article.minmaxComments[1];
+  return Math.round(1 + (((scale - 1) / (max - min)) * nbComments));
+};
+
+module.exports = {
+
+  dateGenerator: function() {
+    var date = new Date();
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"];
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+    date = monthNames[monthIndex] + " " + day + ", " + year;
+    return date;
+  },  
+
+  alreadyExist: function(arr, item) {
+    return (arr.indexOf(item) !== -1);
+  },
+
+  createBelief: function(name, id, opin, src) {
+    var belief = {
+      username: name, // can remove this
+      articleId: id,
+      opinion: opin,
+      source: src,
+      // controversy: ctr, // otherwise need to update values for all users
+      // populartityIdx: popIdx
+    };
+    return belief;
+  },
+
+  createComment: function(id, name, opin, src) {
+    var comment = {
+      articleId: id, // can remove this
+      username: name,
+      opinion: opin,
+      source: src
+    };
+    return comment;
+  },
+
+  getPopularity: function(article, scale) {
+    var nbComments = article.comments.length;
+    var min = article.minmaxComments[0];
+    var max = article.minmaxComments[1];
+    return Math.round(1 + (((scale - 1) / (max - min)) * nbComments));
+  },
+
+  getControversy: function(article, scale) {
+    // contr should not depend on contr of other articles (for the sake of time efficiency)
+    var ctrObj = {};
+    var nbSupported = 0;
+    var nbOffBase = 0;
+    var leastCtr, diff;
+    for (var i = 0; i < article.comments.length; i++) {
+      if (article.comments[i].opinion === "supported") {
+        nbSupported++;
+      } else if (article.comments[i].opinion === "notSupported") {
+        nbOffBase++;
+      }
+    }
+    leastCtr = (nbSupported + nbOffBase);
+    diff = nbSupported - nbOffBase;
+    ctrObj.idx = Math.round((scale / leastCtr) * (leastCtr - Math.abs(diff)));
+    if (diff > 0) {
+      ctrObj.valence = 1;
+    } else if (diff < 0) {
+      ctrObj.valence = -1;
+    } else if (diff === 0) {
+      ctrObj.valence = 0;
+    }
+    return ctrObj;
+  },
+
+
+  articlesCheckOnUpload: function() {
+    return getAllArticles()
+      .then(function (articles) {
+        if (articles.length === 0) {
+          console.log('first article will be created');
+          return [0, 0];
+        /*} else {
+          if (articles[0].minmaxComments[0] !== 0) {
+            console.log('+++++++++++++++++++++++++min comments not 0');
+            // min comments of all the articles has to be changed
+            // update pop idx of all articles
+            updateAllArticlesPopularity(0, 0);
+          }*/
+        } else {
+          return [0, articles[0].minmaxComments[1]]; 
+        }
+      });
+  },
+
+  articlesCheckOnComment: function(article) {
+    var newMax;
+    if (article.comments.length > article.minmaxComments[1]) {
+      newMax = article.comments.length;
+      console.log('++++++++++++++++++++++++there is a new max', newMax);
+      // max comments of all the articles has to be changed
+      // update pop idx of all articles;
+      updateAllArticlesPopularity(1, newMax);
+    // }
+    // or if > min, because this article could have the lowest min, in this case, by adding a comment, the min has to be updated to newMin
+    // if it is also the only article then min has to be evaluated to new min as well
+    // for now we can set the min to always be 0 (to avoid looping though all articles)
+    // then remove check for min in upload
+    // if (article.comments.length > article.minmaxComments[0]) {
+    //   console.log('++++++++++++++++++++++++there may be a new min');
+      // check all the articles length of comments and if they are all >= min then new min
+      
+    } else {
+      newMax = article.minmaxComments[1];
+    }
+    return [article.minmaxComments[0], newMax];
+  }
+
+};
