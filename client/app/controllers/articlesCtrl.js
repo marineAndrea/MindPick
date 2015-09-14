6f0b339af035918c4d189c4aba30533d903e8e7b
Marine Guillanton <marine.guillanton@gmail.com>
2015-09-14 10:04:09 -0700

    removes values from articles view and allows sorting by popularity and controversy

--- client/app/controllers/articlesCtrl.js
+++ client/app/controllers/articlesCtrl.js
@@ -1,12 +1,20 @@
 angular.module('thesis.articles', [])
 
-.controller('ArticlesCtrl', function ($scope, Articles) {
+.controller('ArticlesCtrl', function ($scope, Articles, $filter) {
 
   // Articles.getArticleHTML();
+
+  var orderBy = $filter('orderBy');
+
+  $scope.order = function(predicate, reverse) {
+    $scope.articles = orderBy($scope.articles, predicate);
+  };
+
   $scope.getArticles = function() {
     Articles.getAll()
       .then(function (articles) {
         $scope.articles = articles;
+        $scope.order('-popularityIdx',false);
       })
       .catch(function (error) {
         console.log("oops articles cannot be accessed");
